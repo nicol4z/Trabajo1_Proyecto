bdb3631c3978c083765aaa17b0df372447369ef9
nbarrios <44386526+nicol4z@users.noreply.github.com>
2022-09-01 22:02:14 -0400

    Integracion de proyecto

---
+++ index.py
@@ -0,0 +1,13 @@
+from cgi import test
+from email.mime import application
+from tkinter import *
+
+class Test:
+    def __init__(self, ventana):
+        self.ventana = ventana
+        self.ventana.title("test title");
+
+if __name__ == "__main__":
+    ventana = Tk()
+    application = test(ventana)
+    ventana.mainloop()

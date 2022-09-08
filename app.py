cc58d87c58a473bb00ddd873f604e966223d1d45
nicol4z <nicolas.barrios@alumnos.ucn.cl>
2022-09-08 01:33:58 -0400

    boton para cargar desvinculaciones añadido

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from email.mime import application
 from tkinter import *
 from tkinter import ttk
 import contrataciones
+import desvinculaciones
 
 class app:
    def __init__(self):
@@ -21,7 +22,7 @@ class app:
       labelFrame = LabelFrame(pagina, text = "trabajadores", padx = 0, pady = 10)
       
       labelFrame.grid(column=0, row=0, padx=200, pady=100)
-      buttonCargarContratos = ttk.Button(pagina, text = "cargar contratos", command= lambda: self.CargarContratos())
+      buttonCargarContratos = ttk.Button(pagina, text = "Cargar contratos", command= lambda: self.CargarContratos())
 
       buttonCargarContratos.grid(column=0, row = 0, padx = 0 , pady = 20)
       buttonCargarContratos.place(x = 10 , y = 10 )
@@ -29,11 +30,18 @@ class app:
    
    def mostrarContratos(self, cuaderno):
       pagina = ttk.Frame(cuaderno)
-      cuaderno.add(pagina, text = "mostrar contratos")
+      cuaderno.add(pagina, text = "Desvinculaciones")
+      btnCargarDesv = ttk.Button(pagina, text = "Cargar Desvinculaciones", command = lambda:self.CargarDesvinculaciones())
+      btnCargarDesv.grid(column = 0, row = 0, padx = 0 , pady = 20)
+      btnCargarDesv.place(x = 10 , y = 10)
       
 
    def CargarContratos(self):
       contrataciones.contrataciones.cargarContrataciones()
+   
+   def CargarDesvinculaciones(self):
+      desvinculaciones.Desvinculaciones.cargarDesvinculaciones()
+
       
 
 application = app()
--- desvinculaciones.py
+++ desvinculaciones.py
@@ -5,19 +5,30 @@ from tkinter import filedialog
 from typing import List, Tuple 
 
 
-filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", 
-filetypes = (("Text files","*.csv*"),("all files","*.*")))
+class Desvinculaciones:
+    def cargarDesvinculaciones():
+        filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", 
+        filetypes = (("Text files","*.csv*"),("all files","*.*")))
+        conexion = sqlite3.connect("db1.db")
+        cursor = conexion.cursor()
 
-#archivo = csv.DictReader(filename, delimiter = ";", skipinitialspace = True)
-conexion = sqlite3.connect("db1.db")
-cursor = conexion.cursor()
+        with open(filename) as data:
+            reader = csv.DictReader(data, delimiter=";", skipinitialspace = True)
+            for n, r in enumerate(reader):
+                t = tuple(r.values())
+                cursor.execute("INSERT INTO Desvinculaciones(RUT, DV, NOMBRES, APELLIDO_PATERNO, APELLIDO_MATERNO, NUMERO_CONTRATO, FECHA_DESVINCULACION, CAUSAL) "+
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?);", t)
+                conexion.commit()
+            conexion.close()
 
-with open(filename) as data:
-    reader = csv.DictReader(data, delimiter=";", skipinitialspace = True)
-    for n, r in enumerate(reader):
-        t = tuple(r.values())
-        cursor.execute("INSERT INTO Desvinculaciones(RUT, DV, NOMBRES, APELLIDO_PATERNO, APELLIDO_MATERNO, NUMERO_CONTRATO, FECHA_DESVINCULACION, CAUSAL) "+
-        "VALUES (?, ?, ?, ?, ?, ?, ?, ?);", t)
-        conexion.commit()
+
+
+#with open(filename) as data:
+ #   reader = csv.DictReader(data, delimiter=";", skipinitialspace = True)
+  #  for n, r in enumerate(reader):
+   #     t = tuple(r.values())
+    #    cursor.execute("INSERT INTO Desvinculaciones(RUT, DV, NOMBRES, APELLIDO_PATERNO, APELLIDO_MATERNO, NUMERO_CONTRATO, FECHA_DESVINCULACION, CAUSAL) "+
+     #   "VALUES (?, ?, ?, ?, ?, ?, ?, ?);", t)
+      #  conexion.commit()
         
-conexion.close()
+#conexion.close()
